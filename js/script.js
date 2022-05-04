a5c7082ecba77e2d7b4b7ef550e9acfd8703a923
FERNANDO MIRANDA DA SILVA SANTOS <fernando.mssantos2@senacsp.edu.br>
2022-05-04 17:22:40 -0300

    Finalizando mais uma das partes do site

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function simulador(){
+    const nome = document.getElementById("nome")
+    const cpf = document.getElementById("cpf")
+    const email = document.getElementById("email")
+    const tel = document.getElementById("tel")
+    const entrada = document.getElementById("entrada")
+    const modelo = document.getElementById("modelo")
+
+    if(entrada.value < 3000){
+        return alert("Sua entrada está muito baixa!")
+
+    }
+   
+
+    var calc = modelo.value - entrada.value
+
+    alert(calc)
+
+    var calc12 = Math.round(calc / 12);
+
+    var calc24 = Math.round(calc / 24);
+
+    var calc36 = Math.round(calc / 36);
+
+    var calc48 = Math.round(calc / 48);
+
+    alert(`A sua simulação resultou em\n
+    sua entrada foi de: R$ ${entrada.value}
+    O valor da moto é de: R$ ${modelo.value}
+    O saldo para parcelar é de: R$ ${calc}
+    O valor das parcelas em 12X é de: R$ ${calc12}
+    O valor das parcelas em 24X é de: R$ ${calc24}
+    O valor das parcelas em 36X é de: R$ ${calc36}
+    O valor das parcelas em 48X é de: R$ ${calc48}`)
+
+    
+
+
+
+
+
+
+
+
+}
